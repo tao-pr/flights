12c5d536db7e8d4b1f1635e69e9a8ccc6857dcd0
starcolon <tao@starcolon.com>
2016-04-24 02:26:39 +0700

    {OPENFLIGHTS} Both #flight and #airline work

--- src/main/scala/core.scala
+++ src/main/scala/core.scala
@@ -4,7 +4,10 @@ package starcolon.flights.core
  * Core module of Flights
  */
 object Core {
+	import starcolon.flights.openflights._
   def main(args: Array[String]) {
+  	val airlines = OpenFlights.airlines
 
+		airlines foreach { a => a.prettyPrint() } 
   }
 }
--- src/main/scala/openflights.scala
+++ src/main/scala/openflights.scala
@@ -6,6 +6,10 @@ class Airline (_id: Long, _name: String, _country: String){
 	var id: Long = _id
 	var name: String = _name
 	var country: String = _country
+
+	def prettyPrint(){
+		println("✈️ " + Console.CYAN + name + " " + Console.WHITE + country + Console.RESET)
+	}
 }
 
 class Airport (_code: String, _name: String, _city: String, _country: String, _lat: Float, _lng: Float){
@@ -36,10 +40,16 @@ object OpenFlights{
 	def loadAirports(path: String): List[Airport] = {
 		val records = loadCSVDataFile(path)
 		records.foldLeft(List[Airport]()) { (list, n) =>
-			list ++ List(new Airport(n(4),n(1),n(2),n(3),n(6).toFloat,n(7).toFloat))
+			if (n.length>12)
+				// In case the city has a comma,
+				// it will be unintentionally splitted so we have
+				// one extra element in the splitted array.
+				list ++ List(new Airport(n(4),n(1),n(2)+", "+n(3),n(4),n(7).toFloat,n(8).toFloat))
+			else
+				list ++ List(new Airport(n(4),n(1),n(2),n(3),n(6).toFloat,n(7).toFloat))
 		}
 	}
 
-	val airlines = loadAirlines("../../../data/openflights/airlines.dat")
-	val airports = loadAirports("../../../data/openflights/airports.dat")
+	val airlines = loadAirlines("./data/openflights/airlines.dat")
+	val airports = loadAirports("./data/openflights/airports.dat")
 }
