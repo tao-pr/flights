312d18a0849c80f236c8ed3c36a8b068a6df6eb2
starcolon <tao@starcolon.com>
2016-04-24 12:39:00 +0700

    Add initial geo.scala and routemap.scala

---
+++ src/main/scala/geo.scala
@@ -0,0 +1,7 @@
+package starcolon.flights.geo
+
+object Geo{
+	def distance(lat1: Float, lng1: Float, lat2: Float, lng2: Float): Float{
+	
+	}
+}
---
+++ src/main/scala/routemap.scala
@@ -0,0 +1,10 @@
+package starcolon.flights.routemap
+
+import starcolon.flights.openflights
+
+/**
+ * Route mapper/finder utility
+ */
+object RouteMap{
+	
+}
